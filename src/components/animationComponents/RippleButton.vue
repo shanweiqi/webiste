bf6643cd7536ab2f1ceef52962171415bccc1cb2
shanweiqi <1520822472@qq.com>
2021-06-25 15:41:33 +0800

    feat:波纹按钮

--- src/components/animationComponents/RippleButton.vue
+++ src/components/animationComponents/RippleButton.vue
@@ -1,64 +1,78 @@
 <template>
-    <div>
-        <a href="#" id="bt">Button</a>
-    </div>
+  <div>
+    <a href="#" id="bt" @click="showPopup"
+      >Button
+      <span
+        v-if="rippleSwitch"
+        :style="{ left: rippleLeft, top: rippleTop }"
+      ></span>
+    </a>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'RippleButton',
-    methods:{
-        init(){
-            console.log(document)
-            const button = document.getElementById("bt");
-            button.addEventListener('click',e => {
-                let x = e.clientX - e.target.offsetLeft;
-                let y = e.clientY - e.target.offsetTop;
-
-                let ripple = document.createElement('span');
-                ripple.style.left = x + 'px';
-                ripple.style.top = y + 'px';
-                ripple.className = 'rippleSpan';
-                button.appendChild(ripple);
-            })
-        }
+  name: "RippleButton",
+  data() {
+    return {
+      rippleSwitch: false,
+      rippleTop: 0,
+      rippleLeft: 0,
+    };
+  },
+  methods: {
+    showPopup(e) {
+      // 元素显示
+      this.rippleSwitch = true;
+      // 获取鼠标在元素内点击位置
+      let x = e.offsetX;
+      let y = e.offsetY;
+      this.rippleLeft = x + "px";
+      this.rippleTop = y + "px";
+      // 设置定时器并及时清除
+      var time = setTimeout(() => {
+        // 元素隐藏
+        this.rippleSwitch = false;
+        clearTimeout(time);
+      }, 1000);
     },
-    mounted:function(){
-        this.init();
-    }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-a{
-    position: relative;
-    display: inline-block;
-    padding: 12px 36px;
-    margin: 10px 0;
-    color: #fff;
-    text-decoration: none;
-    text-transform: uppercase;
-    font-size: 18px;
-    letter-spacing: 2px;
-    border-radius: 40px;
-    background: linear-gradient(90deg,#755bea,#ff72c0);
+a {
+  position: relative;
+  display: inline-block;
+  padding: 12px 36px;
+  margin: 10px 0;
+  color: #fff;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-size: 18px;
+  letter-spacing: 2px;
+  border-radius: 40px;
+  overflow: hidden;
+  background: linear-gradient(90deg, #755bea, #ff72c0);
 }
-.rippleSpan{
-    position: absolute;
-    background: #fff;
-    transform: translate(-50%,-50%);
-    pointer-events: none;
-    border-radius: 50%;
-    animation: ripple 1s linear infinite;
+span {
+  position: absolute;
+  background: #fff;
+  transform: translate(-50%, -50%);
+  pointer-events: none;
+  border-radius: 50%;
+  animation: ripple 1s linear infinite;
 }
-@keyframes ripple{
-    0%{
-        width: 0px;
-        height: 0px;
-    }
-    100%{
-        width: 50px;
-        height: 50px;
-    }
+@keyframes ripple {
+  0% {
+    width: 0px;
+    height: 0px;
+    opacity: 0.5;
+  }
+  100% {
+    width: 500px;
+    height: 500px;
+    opacity: 0;
+  }
 }
 </style>
